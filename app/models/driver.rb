89f5453521ee1afa964c2466f46c4879c2489ae3
Khang Le <khangsile@gmail.com>
2013-09-22 20:42:36 -0400

    made one-to-one relationship with user, driver; generate test for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,7 @@
 class Driver < ActiveRecord::Base
+
+belongs_to :user
+
+validates :user_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :driver
   has_many :user_reviews, as: :reviewer
   has_many :user_reviews, as: :reviewee
 
