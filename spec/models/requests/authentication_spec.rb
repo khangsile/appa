e471703650ea0907342326917e0411aeae94b02c
tsle <les.tuan@gmail.com>
2013-09-24 02:18:01 -0400

    Add authentication tests

---
+++ spec/models/requests/authentication_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+	subject { page }
+
+	describe "sign in page" do
+		before { visit new_user_session_path }
+
+		it { should have_content("Sign in") }
+
+		let(:user) { FactoryGirl.create(:user) }
+
+		describe "with invalid information" do
+			before do
+				fill_in "Email", with: user.email.upcase
+				fill_in "Password", with: user.password + "j"
+				click_button "Sign in"
+			end
+
+			it { should have_selector("div.alert.alert-error") }
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "Email", with: user.email.upcase
+				fill_in "Password", with: user.password
+				click_button "Sign in"
+			end
+
+			it { should have_link('Sign out'), destroy_user_session_path }
+		end		
+			
+  end
+end
