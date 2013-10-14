24c43d0d22ec44498941eadca3db086cf50cf6de
tsle <les.tuan@gmail.com>
2013-10-14 16:26:00 -0400

    Add driver search query

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -10,5 +10,20 @@ class Driver < ActiveRecord::Base
 	validates :user_id, presence: true
 	validates :balance, numericality: { greater_than_or_equal_to: 0 }
 
+	def self.within(left, bottom, right, top)
+		where(%{
+			Drivers.location && ST_MakeEnvelope(%f, %f, %f, %f, 4326)
+			} % [left,bottom,right,top])
+	end
+
+	def self.within(longtidue, latitude, distance)
+		where(%{
+			ST_DWithin(
+				Drivers.location,
+				ST_GeographyFromText('SRID=4326;POINT(%f, %f)'),
+				%d
+			)
+			} % [longtitude, latitude, distance])
+	end
 
 end
